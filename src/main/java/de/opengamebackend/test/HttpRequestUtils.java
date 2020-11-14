ce3ed99ebd569d19f7356fe3c2d25dce754d0d76
Nick Prühs <dev@npruehs.de>
2020-11-14 01:00:56 +0100

    Add support for GET requests and LocalDateTime serialization.

--- src/main/java/de/opengamebackend/test/HttpRequestUtils.java
+++ src/main/java/de/opengamebackend/test/HttpRequestUtils.java
@@ -1,16 +1,31 @@
 package de.opengamebackend.test;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 public class HttpRequestUtils {
+    public <T> T assertGetOk(MockMvc mvc, String url, Class<T> responseClass) throws Exception {
+        String responseJson = mvc.perform(get(url)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andReturn()
+                .getResponse().getContentAsString();
+
+        T response = createObjectMapper().readValue(responseJson, responseClass);
+        assertThat(response).isNotNull();
+
+        return response;
+    }
+
     public <T> T assertPostOk(MockMvc mvc, String url, Object request, Class<T> responseClass) throws Exception {
-        ObjectMapper objectMapper = new ObjectMapper();
+        ObjectMapper objectMapper = createObjectMapper();
         String requestJson = objectMapper.writeValueAsString(request);
 
         String responseJson = mvc.perform(post(url)
@@ -24,5 +39,14 @@ public class HttpRequestUtils {
         assertThat(response).isNotNull();
 
         return response;
+    }
+
+    private ObjectMapper createObjectMapper() {
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        // Required for proper JSON (de-)serialization of LocalDateTime.
+        objectMapper.registerModule(new JavaTimeModule());
+
+        return objectMapper;
     }
 }
